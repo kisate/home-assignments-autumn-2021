896d152cf555d2a8b104ea5bd034e9a0e792d238
Dmitrii <dhkas@mail.ru>
2021-10-03 22:48:16 +0300

    task done

--- camtrack/corners.py
+++ camtrack/corners.py
@@ -48,15 +48,55 @@ class _CornerStorageBuilder:
 
 def _build_impl(frame_sequence: pims.FramesSequence,
                 builder: _CornerStorageBuilder) -> None:
+    
+    max_corners = 3000
+
+    params = {
+        "qualityLevel": 0.03,
+        "blockSize": 10,
+        "minDistance": 10
+    }
+
     # TODO
     image_0 = frame_sequence[0]
+    
+    corners = cv2.goodFeaturesToTrack(image_0, maxCorners=max_corners, **params)
+
     corners = FrameCorners(
-        np.array([0]),
-        np.array([[0, 0]]),
-        np.array([55])
+        np.arange(corners.shape[0]),
+        corners,
+        np.array([10]*corners.shape[0])
     )
+
     builder.set_corners_at_frame(0, corners)
     for frame, image_1 in enumerate(frame_sequence[1:], 1):
+        new_corners, st, err = cv2.calcOpticalFlowPyrLK((image_0 * 255).astype(np.uint8), (image_1 * 255).astype(np.uint8), corners.points, None, winSize=(30,30), maxLevel=2, criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT,
+                              10, 0.03))
+
+        inds = (st == 1).reshape(-1)
+        good_corners = new_corners[inds]
+
+        mask = np.ones(image_1.shape).astype(np.uint8)
+        for corner in good_corners:
+            cv2.circle(mask, (int(corner[0]), int(corner[1])), 100, 0)
+
+        ids = corners.ids[inds]
+
+        if sum(inds) < max_corners:
+            extra_corners = cv2.goodFeaturesToTrack(image_1, maxCorners=max_corners-sum(inds), mask=mask, **params)
+            starting_id = max(ids) + 1
+            ids = np.concatenate((ids, np.arange(starting_id, starting_id + extra_corners.shape[0]).reshape(-1, 1)), axis=0)
+            good_corners = np.concatenate((good_corners.reshape((-1, 1, 2)), extra_corners), axis=0)
+
+
+        corners = FrameCorners(
+            ids,
+            good_corners,
+            np.array([10]*good_corners.shape[0])
+        )
+        
+
+
         builder.set_corners_at_frame(frame, corners)
         image_0 = image_1
 
